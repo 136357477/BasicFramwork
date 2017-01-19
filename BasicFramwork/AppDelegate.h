7d6b1e885495aca6c2fdb41594d1d04d78f06ad3
guohui <guohui136357477@126.com>
2017-01-19 15:27:10 +0800

    Initial Commit

---
+++ BasicFramwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicFramwork
+//
+//  Created by guohui on 2017/1/19.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicFramwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicFramwork
+//
+//  Created by guohui on 2017/1/19.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
